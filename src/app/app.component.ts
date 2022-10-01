21ccf4c341287f7712516d650912fd11352df20c
DESKTOP-UAD63F6\Eva <evaradu@hotmail.fr>
2022-10-01 17:08:29 +0200

    using services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.models';
 
 @Component({
   selector: 'app-root',
@@ -8,63 +7,7 @@ import { FaceSnap } from './models/face-snap.models';
 })
 export class AppComponent implements OnInit {
 
-  faceSnaps!: FaceSnap[];
 
 
-  ngOnInit(){
-
-    this.faceSnaps = [ 
-     {
-        title: "Un Chat",
-        description: "Miaou",
-        imageUrl: "https://spaestrie.qc.ca/wp-content/uploads/2021/04/Gabarit_Blogue-3.png",
-        createdDate: new Date(),
-        snaps: 350,
-        location: "Nice"
-      },
-      {
-        title: "Un Chien",
-        description: "Woof",
-        imageUrl: "https://www.woopets.fr/assets/img/001/244/1200x675/choisir-chien-japonais.jpg",
-        createdDate: new Date(),
-        snaps: 6,
-        location: "Le parc"
-      },
-      {
-        title: "Un Chaton",
-        description: "Meow",
-        imageUrl: "https://www.planeteanimaux.com/wp-content/uploads/2020/10/eduquer-un-chaton.jpg",
-        createdDate: new Date(),
-        snaps: 0
-      },
-      {
-        title: "Un Chat",
-        description: "Miaou",
-        imageUrl: "https://spaestrie.qc.ca/wp-content/uploads/2021/04/Gabarit_Blogue-3.png",
-        createdDate: new Date(),
-        snaps: 0,
-        location: "Nice"
-      },
-      {
-        title: "Un Chien",
-        description: "Woof",
-        imageUrl: "https://www.woopets.fr/assets/img/001/244/1200x675/choisir-chien-japonais.jpg",
-        createdDate: new Date(),
-        snaps: 6,
-        location: "Le parc"
-      },
-      {
-        title: "Un Chaton",
-        description: "Meow",
-        imageUrl: "https://www.planeteanimaux.com/wp-content/uploads/2020/10/eduquer-un-chaton.jpg",
-        createdDate: new Date(),
-        snaps: 0
-      }
-    ];
-    
-
-  
-
-   
-  }
+  ngOnInit(){ }
 }
---
+++ src/app/services/face-snaps.service.ts
@@ -0,0 +1,84 @@
+import { Injectable } from "@angular/core";
+import { FaceSnap } from "../models/face-snap.models";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class FaceSnapsService {
+    faceSnaps: FaceSnap[] = [ 
+        {
+           id: 1,
+           title: "Un Chat",
+           description: "Miaou",
+           imageUrl: "https://spaestrie.qc.ca/wp-content/uploads/2021/04/Gabarit_Blogue-3.png",
+           createdDate: new Date(),
+           snaps: 350,
+           location: "Nice"
+         },
+         {
+           id: 2,
+           title: "Un Chien",
+           description: "Woof",
+           imageUrl: "https://www.woopets.fr/assets/img/001/244/1200x675/choisir-chien-japonais.jpg",
+           createdDate: new Date(),
+           snaps: 6,
+           location: "Le parc"
+         },
+         {
+           id: 3,
+           title: "Un Chaton",
+           description: "Meow",
+           imageUrl: "https://www.planeteanimaux.com/wp-content/uploads/2020/10/eduquer-un-chaton.jpg",
+           createdDate: new Date(),
+           snaps: 0
+         },
+         {
+           id: 4,
+           title: "Un Chat",
+           description: "Miaou",
+           imageUrl: "https://spaestrie.qc.ca/wp-content/uploads/2021/04/Gabarit_Blogue-3.png",
+           createdDate: new Date(),
+           snaps: 0,
+           location: "Nice"
+         },
+         {
+           id: 5,
+           title: "Un Chien",
+           description: "Woof",
+           imageUrl: "https://www.woopets.fr/assets/img/001/244/1200x675/choisir-chien-japonais.jpg",
+           createdDate: new Date(),
+           snaps: 6,
+           location: "Le parc"
+         },
+         {
+           id: 6,
+           title: "Un Chaton",
+           description: "Meow",
+           imageUrl: "https://www.planeteanimaux.com/wp-content/uploads/2020/10/eduquer-un-chaton.jpg",
+           createdDate: new Date(),
+           snaps: 0
+         }
+       ];
+
+       getAllFaceSnaps(): FaceSnap[]{
+        return this.faceSnaps;
+       }
+
+       getFaceSnapById(faceSnapId: number) : FaceSnap{
+        const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId);
+        if (!faceSnap){
+            throw new Error('FaceSnap not found!');
+        }
+        else{
+            return faceSnap;
+        }
+       }
+
+    snapFaceSnapById(faceSnapId: number, snapType: 'snap' | 'unsnap'): void{
+        const faceSnap = this.getFaceSnapById(faceSnapId);
+        snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
+    }
+
+
+
+}
